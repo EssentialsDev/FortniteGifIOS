c6f08e606f361a76dcab7e640f02049225af5b66
Kasey Schlaudt <kasey.schlaudt@life.church>
2019-02-09 18:43:09 -0600

    Initial Commit

---
+++ FortniteGif2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FortniteGif2
+//
+//  Created by Kasey Schlaudt on 2/9/19.
+//  Copyright © 2019 Kasey Schlaudt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
